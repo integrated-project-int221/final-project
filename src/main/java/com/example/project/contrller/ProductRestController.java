35bdfa346c8ddf218fb2ba50fca3372b53696276
tein1142 <tein1142@hotmail.com>
2021-04-24 21:32:52 +0700

    [ADD]Get home.html

--- src/main/java/com/example/project/contrller/ProductRestController.java
+++ src/main/java/com/example/project/contrller/ProductRestController.java
@@ -4,9 +4,11 @@ import com.example.project.model.Products;
 import com.example.project.repositories.ColorRepositories;
 import com.example.project.repositories.ProductRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/products")
@@ -17,10 +19,7 @@ public class ProductRestController {
     @Autowired
     private ProductRepositories productRepositories;
 
-    @GetMapping("/")
-        public String getHome(){
-        return "home";
-    }
+
 
     @GetMapping("/items")
     public List<Products> getAllProduct(){

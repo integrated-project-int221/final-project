3486f51ebc2830461add650bb60dd9374eee31ca
tein1142 <59915615+tein1142@users.noreply.github.com>
2021-04-24 22:02:32 +0700

    Update get home

--- src/main/java/com/example/project/contrller/PageController.java
+++ src/main/java/com/example/project/contrller/PageController.java
@@ -3,12 +3,12 @@ package com.example.project.contrller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@RestController
 public class PageController {
 
     @GetMapping("/")
     public String getHome(){
-        return "home";
+        return "Test Deploy SpringBoot RestAPI";
     }
 
 }

1aa8e2b46c370b3a72154e19a0b56c6410117928
Guitar_SPK <Supphakit.tar@mail.kmutt.ac.th>
2021-05-01 23:59:21 +0700

    'add-column-imagename'

--- 2.Insert-Data.sql
+++ 2.Insert-Data.sql
@@ -13,57 +13,57 @@ insert into myproduct.colors(colorid,colorname,colorvalue) values(4,'Wood','#996
 insert into myproduct.colors(colorid,colorname,colorvalue) values(5,'Green','#69C08A');
 insert into myproduct.colors(colorid,colorname,colorvalue) values(6,'Black','#000000');
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values (1,'D-12E KOA',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values (1,'D-12E KOA',
 'A great bang for your buck Martin packed with features: here the D-12E, a solid wood Dreadnought built with sitka spruce for the top and fantastic looking koa veneer back and sides. This all-new model is set apart from its siblings with a Style 28 multi-stripe rosette and Style 18 purfling on the back. It has an FSC® Certified Richlite® fingerboard and bridge, gloss body, and a hand-rubbed neck finish. Like other Road Series models, it also has Fishman® MX-T electronics with a built-in soundhole tuner that auto mutes the audio output so you can tune up any time without using a pedal.',
-49000,'2021-01-01',1);
+49000,'2021-01-01','Guitar.png',1);
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values (2,'D17M',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values (2,'D17M',
 'Martin D-17M Guitar Resurrecting the look of our early Style 17 guitars, the all solid-wood 17 Series features a shaded gloss Sitka Spruce top, solid mahogany back and sides, and tortoise brown binding. The D-17M features a booming Dreadnought body, great for banging out rockers or quiet fingerpicking.',
-46900,'2021-01-01',1);
+46900,'2021-01-01','Guitar.jpg',1);
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values(3,'DX JOHNNY CASH',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values(3,'DX JOHNNY CASH',
 'Johnny Cash was famous for his one-of-kind baritone voice, distinctive all-black wardrobe, and — as acoustic guitar aficionados will attest — his beloved Martin Dreadnought. The Martin DX Johnny Cash supplies you with a stylish facsimile of his signature axe, at a price that’s sure make any guitarist smile ear to ear. This acoustic-electric guitar boasts the unmistakable Martin feel and tonality, along with a comfortable Performing Artist-profile neck and Fishman electronics for top-notch plugged-in tone. The Johnny Case DX A/E includes an eye-catching black satin finish, custom “autographed” rosette, and star fingerboard inlays with CASH at the 17th fret — it’s a must-have collectable for all Johnny Cash fans.',
-23800,'2021-01-01',1);
+23800,'2021-01-01','Guitar.png',1);
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values(4,'HD-200-LEFT HAND',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values(4,'HD-200-LEFT HAND',
 'MODEL HD-200/FS.N BODY SHAPE Dreadnought TOP Engelmann Spruce BACK & SIDES Mahogany NECK Mahogany FINGERBOARD & BRIDGE EboTech FINGERBOARD INLAY Dot Position Marks SOUNDHOLE INLAY WD-112 Wood-look Decal TUNERS Chrome Tuners ELECTRONICS STRINGS Light (.012 - .053) COLORS Natural FINISH Satin SCALE 647.7 ㎜ / 25.5 inches',
-4800,'2021-02-02',2);
+4800,'2021-02-02','Guitar.png',2);
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values(5,'STG G-18CE',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values(5,'STG G-18CE',
 'Crafter Grand Auditorium guitars are crafted with both comfort and sound in mind. The sloped shoulder allows for easy finger picking, and the full size body delivers the projection you would expect from a dreadnaught. Our acoustic electric Grand Auditoriums have a cutaway body, allowing better access for playing past the 12th fret.',
-19600,'2021-02-02',2);
+19600,'2021-02-02','Guitar.png',2);
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values(6,'STG J-18CE',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values(6,'STG J-18CE',
 'Big bodies for a big sound. Our Jumbo Six and Twelve String guitars deliver everything you want in a jumbo body, fantastic volume and a big bass response. Available with solid spruce or cedar tops, and a rosewood or tiger maple back and sides.',
-19500,'2021-02-02',2);
+19500,'2021-02-02','Guitar.png',2);
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values(7,'214CE-FIGURED OVANGOL DLX LTD',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values(7,'214CE-FIGURED OVANGOL DLX LTD',
 'The Taylor 241ce Deluxe Acoustic-electric Guitar adds a lively tonal streak to Taylor 200 series. This acoustic-electric is made with a sitka spruce top and figured ovangkol back and sides. You will enjoy the visual appeal and impressive tone that pleases the ear. It has a well-rounded acoustic character and showcases noteworthy note definition. Plug into the ES-2 electronics, and the true acoustic character will be transferred to your PA without the potential for sounding overly thin and compressed as other acoustic-electrics can. The 214ce Deluxe has a tropical mahogany neck with Taylor standard carve. You will fall in love with the way the neck fills your hand without hampering your playing. Whether you enjoy fingerstyle or dig in with a flat pick, the Taylor 214ce Deluxe is perfectly suited for anything you throw at it.',
-49700,'2021-03-03',3);
+49700,'2021-03-03','Guitar.png',3);
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values (8,'327E V-CLASS',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values (8,'327E V-CLASS',
 'With its unique round-shoulder dreadnought silhouette, revolutionary V-Class Bracing, and dramatically different tonal personality, Taylor 327e Grand Pacific blazes new ground for Taylor. Featuring Tasmanian blackwood back and sides and a tropical mahogany top, this alluring acoustic-electric guitar delivers a seductively bold and versatile tonal profile that is perfectly amplified by the onboard ES2 electronics. Boasting the understated elegance of Taylor 3 Series, the 327e Grand Pacific is graced by a curvaceous pickguard, 3-ring rosette, and beautiful Italian acrylic Gemstone fingerboard inlays. Its tropical mahogany neck and ebony fingerboard play flawlessly. The 327e Grand Pacific is an amazing guitar and an incredible value.',
-87500,'2021-03-03',3);
+87500,'2021-03-03','Guitar.png',3);
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values (9,'ACADEMY 12 LEFT HAND',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values (9,'ACADEMY 12 LEFT HAND',
 'Taylor Academy Series makes the playing experience more accessible in every way, packing signature Taylor playing features into an affordable package. With the A12, Taylor compact Grand Concert body makes the perfect companion for fingerpicking or anyone with a lighter touch who craves the physical comfort of a small-body guitar. The guitar’s design specs are optimized for playing ease.',
-18900,'2021-03-03',3);
+18900,'2021-03-03','Guitar.png',3);
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values (10,'F-310',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values (10,'F-310',
 'Yamaha F-310 Guitar is an Acoustic guitar.It is a original guitar from Yamaha. This guitar have a good sound to played.Who is the beginner want to practice guitar.It very suitably to buy it.',
-4290,'2021-04-04',4);
+4290,'2021-04-04','Guitar.png',4);
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values (11,'FS800',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values (11,'FS800',
 'Get incredible tone and playability at an amazing price point with the Yamaha FS800 Concert Acoustic. Featuring a solid spruce top with laminated back and sides, this stellar instrument puts out incredible tone and impressive projection. The smaller concert size of the FS800 is ideal for beginning and emerging players but also a popular choice for finger-picking and recording. Its nato neck capped with walnut is incredibly comfortable and playable — great for chording and lead playing. In addition, scalloped X-bracing provides further support and tonal enhancement — a feature we at Sweetwater really appreciate.',
-6210,'2021-04-04',4);
+6210,'2021-04-04','Guitar.png',4);
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values (12,'SLG200S',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values (12,'SLG200S',
 'With the SLG200S, Yamaha has solved a headache traveling guitarists have always had to deal with: finding space for your instrument. Acoustic guitars, because of their bulk, are especially problematic. The SLG200S slim body breaks down to fit in its included compact gig bag. It will easily fit in the overhead compartment on your flight, under the seat in the band van, or anywhere else it needs to.',
-21815,'2021-04-04',4);
+21815,'2021-04-04','Guitar.png',4);
 
-insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,bands_bandid) values (13,'CUSTOM 24 35TH CHARCOAL CHERRY BURST',
+insert into myproduct.products(prodcode,prodname,proddescription,price,prodmanufactured,imagename,bands_bandid) values (13,'CUSTOM 24 35TH CHARCOAL CHERRY BURST',
 'The 35th Anniversary Custom 24 is here to celebrate 35 years of guitar making and innovation. Designed around the guitar that started it all, the Custom 24, this limited-edition model captures the foundational elements of the iconic model and adds the musical functionality of the Paul’s Guitar, creating a new, versatile platform. The 35th Anniversary Custom 24 features 85/15 treble and bass pickups that are paired with two mini-toggle switches that allow players to put either or both pickups in humbucking or true single-coil mode. The clarity and unique tones of these vintage-inspired pickups are both big and nuanced, so players can easily find their sweet spot. Join PRS in celebrating our ongoing journey with the 35th Anniversary Custom 24.  ',
-135800,'2020-05-05',5);
+135800,'2020-05-05','Guitar.png',5);
 
 
 insert into myproduct.productcolor(colorid,prodcode) values(1,1);
@@ -79,3 +79,4 @@ insert into myproduct.productcolor(colorid,prodcode) values(4,4);
 insert into myproduct.productcolor(colorid,prodcode) values(5,4);
 insert into myproduct.productcolor(colorid,prodcode) values(6,4);
 insert into myproduct.productcolor(colorid,prodcode) values(1,5);
+

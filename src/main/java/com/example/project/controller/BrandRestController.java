cd4185643adc9cf23459a8a8fca153cb7384f81b
tein1142 <tein1142@hotmail.com>
2021-05-02 01:44:25 +0700

    [Update] Controllor

--- src/main/java/com/example/project/controller/BrandRestController.java
+++ src/main/java/com/example/project/controller/BrandRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins="http://localhost:8081/")
+@CrossOrigin(origins="http://localhost:8081")
 @RestController
 @RequestMapping("/brands")
 public class BrandRestController {
--- src/main/java/com/example/project/controller/ProductRestController.java
+++ src/main/java/com/example/project/controller/ProductRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins="http://localhost:8081/")
+@CrossOrigin(origins="http://localhost:8081")
 @RestController
 @RequestMapping("/products")
 public class ProductRestController {

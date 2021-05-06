8fd2edd1ef987b9c71ab68ec23008fc75b6dacbd
tein1142 <tein1142@hotmail.com>
2021-05-06 17:51:32 +0700

    [Update] ImagesController

--- src/main/java/com/example/project/controller/ImageRestController.java
+++ src/main/java/com/example/project/controller/ImageRestController.java
@@ -42,13 +42,13 @@ public class ImageRestController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<Object> imageUpload(@RequestParam("File") MultipartFile file,@RequestParam Products Product) throws IOException {
+    public ResponseEntity<Object> imageUpload(@RequestParam("File") MultipartFile file,@RequestParam String productName) throws IOException {
         try{
 //        String fileType = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
         Path path = Paths.get(FILE_DIRECTORY);
         File dir = new File(FILE_DIRECTORY);
         if (!dir.exists()) Files.createDirectories(path);
-            File imageFile = new File(FILE_DIRECTORY + Product.getProdName() + file.getOriginalFilename()) ;
+            File imageFile = new File(FILE_DIRECTORY + productName + file.getOriginalFilename()) ;
 //            String imageName = productRepositories.findById(id).get().getProdName() + fileType;
             imageFile.createNewFile();
             FileOutputStream fos = new FileOutputStream(imageFile);
